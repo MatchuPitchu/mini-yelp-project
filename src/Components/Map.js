7c8439ceccd05f0a65a1bbe58d362e01f96df868
linhg91 <mlinh.ng91@gmail.com>
2021-03-18 16:05:22 +0100

    Map component added with custom marker

---
+++ src/Components/Map.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { Icon } from "leaflet";
+import markerCustom from '../Assets/Img/MarkerIcon.png';
+import "leaflet/dist/images/marker-shadow.png";
+
+import '../Custom Css/Leaflet.css';
+import "leaflet/dist/leaflet.css";
+
+
+const Map = () => {
+  const icon = new Icon({
+    iconUrl: markerCustom,
+    iconSize: [35, 35],
+  });
+
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker icon={icon} position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+  )
+}
+
+export default Map
