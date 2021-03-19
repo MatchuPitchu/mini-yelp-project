413005229240c78f55e564520dbbb05ce3676c6b
Matchu Pitchu <77695211+MatchuPitchu@users.noreply.github.com>
2021-03-19 12:44:24 +0100

    Add Search functionality on Landing Page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import {Switch, Route} from 'react-router-dom';
 import Navbar from "./Navbar/Navbar";
-import Search from './Search/Search';
 import Map from "./Map/Map";
 import RestaurantCards from './RestaurantCards/RestaurantCards';
+import RestaurantCardsSearch from './RestaurantCardsSearch/RestaurantCardsSearch';
 import Footer from "./Footer/Footer";
 import { dom } from "@fortawesome/fontawesome-svg-core";
 // Import context - fetched Data
@@ -20,15 +20,19 @@ const App = () => {
   return (
     <div>
       <header>
-        <Switch>
-          <Route path='/search' component={Search}/>
-          <Route path='/' component={Navbar}/>
-        </Switch>
+        <Route path='/' component={Navbar}/>
       </header>
       <body className="container">
         <div className="row">
           <div className="col-8 mt-5 mb-4">
-            <RestaurantCards />
+            <Switch>
+              <Route exact path="/">
+                <RestaurantCards />
+              </Route>
+              <Route path="/search">
+                <RestaurantCardsSearch />
+              </Route>
+            </Switch>
           </div>
           <div className="col-4 mt-5 mb-4">
             <Map />
